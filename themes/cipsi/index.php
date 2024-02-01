dde75432956c123189a287390b2881708be51474
ElvijsKurss <elvijs.kurss@gmail.com>
2024-02-01 11:43:57 +0200

    small fix

--- themes/cipsi/index.php
+++ themes/cipsi/index.php
@@ -2,8 +2,27 @@
 
 get_header();
 ?>
-<div id="test-id-two">
-	<a href="https://oveikals.lv/veikals/cipsi-salas-uzkodas/"> <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/Desktop.jpg" /></a>
-</div>
+	<?php
+	// Check if we are in the 'home' section on the front page
+	if (is_home() && is_front_page()) {
+		// Include template-parts/content-home.php
+		get_template_part('template-parts/content', 'home');
+	} else {
+		// Check if we have posts
+		if (have_posts()) {
+			// Loop through posts
+			while (have_posts()) {
+				// Fetch next post
+				the_post();
+
+				// Include template based on post type in template-parts/content-POSTTYPE.php
+				get_template_part('template-parts/content', get_post_type());
+			}
+		} else {
+			// Include content for a page without posts (probably something like 'not found')
+			get_template_part('template-parts/content', 'none');
+		}
+	}
+	?>
 <?php
 get_footer();
---
+++ themes/cipsi/template-parts/content-home.php
@@ -0,0 +1,3 @@
+<div id="test-id-two">
+    <a href="https://oveikals.lv/veikals/cipsi-salas-uzkodas/"> <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/Desktop.jpg" /></a>
+</div>
